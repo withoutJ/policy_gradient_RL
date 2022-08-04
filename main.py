58e2ab6f71c6605c78cdd0dfca77f35ce0069d84
withoutJ <momcilo02@gmail.com>
2022-08-04 18:11:36 +0200

    calculating discount reward

--- main.py
+++ main.py
@@ -4,17 +4,28 @@ from reinforce import Actor
 from simulation import simulate
 
 def main():
-    params = {"num_episodes": 100, "std": 0.5}
+    params = {"num_episodes": 100, "std": 0.5, "gamma": 0.9}
 
     actor = Actor()
     env = gym.make('InvertedPendulum-v4')
-
 
     episode_scores = []
     episode_idx = 0
 
     while episode_idx < params["num_episodes"]:
         episode, episode_score = simulate(env, actor)
+        episode_scores.append(episode_score)
+
+        actor.train(episode)
+
+        gamma = params["gamma"]
+
+        for k in len(episode):
+            
+            for step in episode[k:]:
+                
+
+
         episode_idx+=1
 
     
