dc0673099fc038fc9b06c5cf5fb793b154fca314
Una Jacimovic <unajacimovic@Unas-MacBook-Air.local>
2022-08-04 17:21:58 +0200

    added Policy Network

---
+++ policy.py
@@ -0,0 +1,69 @@
+import numpy
+import torch
+import gym
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+class GaussianPolicy(nn.Module):
+
+    def __init__(self,input_shape,output_shape,hidden_dim,std = 0,stdNetwork = False):
+
+        super().__init__()
+
+        self.layer1 = nn.Linear(input_shape,hidden_dim)
+        self.outputLayer1 = nn.Linear(hidden_dim,output_shape)
+        self.outputLayer2 = None
+        self.std = None
+        self.stdNetwork = stdNetwork
+
+        if stdNetwork:
+            self.outputLayer2 = nn.Linear(hidden_dim,output_shape)
+        else:
+            self.std = std
+
+
+    def forward(self, input):
+
+        mean = self.layer1(input)
+        mean = F.relu(mean)
+        mean = self.outputLayer1(mean)
+
+        if self.stdNetwork:
+            std = self.layer1(input)
+            std = self.outputLayer2(std)
+        else:
+            std = self.std
+
+        distribution = Normal(mean, std)
+        action = distribution.sample()
+
+        log_pb  = distribution.log_prob(action)
+        action = torch.tanh(action)
+        action = action*3
+
+        std = torch.Tensor(std)
+
+        return mean, std, action, log_pb
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
